bb5da269a0340197e97e85a3a34f116e0946722c
sfrancesny <134027005+sfrancesny@users.noreply.github.com>
2023-08-20 19:59:31 -0400

    altered code to reflect ES module

--- miniature-eureka-main/miniature-eureka-main/Develop/server.js
+++ miniature-eureka-main/miniature-eureka-main/Develop/server.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const fs = require("fs");
-const path = require("path");
+import express from 'express';
+import fs from 'fs';
+import path from 'path';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,31 +8,31 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // HTML Routes
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../index.html")); // level to reach the project root
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../index.html')); // level to reach the project root
 });
 
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../notes.html")); // two levels to reach the project root
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../notes.html')); // two levels to reach the project root
 });
 
 // API Routes
-app.get("/api/notes", (req, res) => {
-  const notesData = JSON.parse(fs.readFileSync("db.json", "utf8"));
+app.get('/api/notes', (req, res) => {
+  const notesData = JSON.parse(fs.readFileSync('db.json', 'utf8'));
   res.json(notesData);
 });
 
-app.post("/api/notes", (req, res) => {
+app.post('/api/notes', (req, res) => {
   const newNote = req.body;
-  const notesData = JSON.parse(fs.readFileSync("db.json", "utf8"));
+  const notesData = JSON.parse(fs.readFileSync('db.json', 'utf8'));
 
   newNote.id = notesData.length + 1;
   notesData.push(newNote);
 
-  fs.writeFileSync("db.json", JSON.stringify(notesData));
+  fs.writeFileSync('db.json', JSON.stringify(notesData));
   res.json(newNote);
 });
 
