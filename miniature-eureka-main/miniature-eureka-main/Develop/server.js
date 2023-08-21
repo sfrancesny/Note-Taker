a785dc68345d2cba386428106ffb0ba51731e1ce
sfrancesny <134027005+sfrancesny@users.noreply.github.com>
2023-08-20 20:32:35 -0400

    use the import.meta.url object

--- miniature-eureka-main/miniature-eureka-main/Develop/server.js
+++ miniature-eureka-main/miniature-eureka-main/Develop/server.js
@@ -8,31 +8,35 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+
+// Calculate the directory name based on the current module's URL
+const currentDir = path.dirname(new URL(import.meta.url).pathname);
+
+app.use(express.static(path.join(currentDir, 'public')));
 
 // HTML Routes
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(currentDir, 'public', 'index.html'));
 });
 
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+  res.sendFile(path.join(currentDir, 'public', 'notes.html'));
 });
 
 // API Routes
 app.get('/api/notes', (req, res) => {
-  const notesData = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json'), 'utf8'));
+  const notesData = JSON.parse(fs.readFileSync(path.join(currentDir, 'db.json'), 'utf8'));
   res.json(notesData);
 });
 
 app.post('/api/notes', (req, res) => {
   const newNote = req.body;
-  const notesData = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json'), 'utf8'));
+  const notesData = JSON.parse(fs.readFileSync(path.join(currentDir, 'db.json'), 'utf8'));
 
   newNote.id = notesData.length + 1;
   notesData.push(newNote);
 
-  fs.writeFileSync(path.join(__dirname, 'db.json'), JSON.stringify(notesData));
+  fs.writeFileSync(path.join(currentDir, 'db.json'), JSON.stringify(notesData));
   res.json(newNote);
 });
 
