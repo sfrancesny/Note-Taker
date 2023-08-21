5d9461812408768c37cb2c6ffaaa770d66bd57aa
sfrancesny <134027005+sfrancesny@users.noreply.github.com>
2023-08-20 21:08:37 -0400

    updated server.js file

--- miniature-eureka-main/miniature-eureka-main/Develop/server.js
+++ miniature-eureka-main/miniature-eureka-main/Develop/server.js
@@ -1,9 +1,13 @@
+
 import express from 'express';
+import cors from 'cors'; // Import the cors middleware
 import fs from 'fs';
 import path from 'path';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors()); // Use the cors middleware
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
