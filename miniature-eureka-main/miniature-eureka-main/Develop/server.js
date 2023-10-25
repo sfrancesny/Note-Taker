36abb6b6f87a4794cf20d72b46fa0fa854a46206
sfrancesny <134027005+sfrancesny@users.noreply.github.com>
2023-10-25 01:36:20 -0400

    imported uuid / added a dbFile path added  a fallback route

--- miniature-eureka-main/miniature-eureka-main/Develop/server.js
+++ miniature-eureka-main/miniature-eureka-main/Develop/server.js
@@ -2,19 +2,22 @@ import express from 'express';
 import cors from 'cors'; // Import the cors middleware
 import fs from 'fs';
 import path from 'path';
+// import the uuid library for unique IDs
+import { v4 as uuidv4 } from 'uuid';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors()); // Use the cors middleware
+app.use(cors()); // uses the cors middleware
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Calculate the directory name based on the current module's URL
+
 const currentDir = path.dirname(new URL(import.meta.url).pathname);
-const publicDir = path.join(currentDir, 'public'); // Path to the public folder
+const publicDir = path.join(currentDir, 'public'); // path to the public folder
+const dbFilePath = path.join(currentDir, 'db.json'); // added dbFilePath
 
 app.use(express.static(publicDir));
 
@@ -37,11 +40,16 @@ app.post('/api/notes', (req, res) => {
   const newNote = req.body;
   const notesData = JSON.parse(fs.readFileSync(dbFilePath, 'utf8'));
 
-  newNote.id = notesData.length + 1;
+  newNote.id = uuidv4();  // generates a unique ID
   notesData.push(newNote);
 
   fs.writeFileSync(dbFilePath, JSON.stringify(notesData));
   res.json(newNote);
+});
+
+// Fallback route
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicDir, 'index.html'));
 });
 
 // Start server
